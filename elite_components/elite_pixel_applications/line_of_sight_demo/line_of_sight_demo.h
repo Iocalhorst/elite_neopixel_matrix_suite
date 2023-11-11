a0f9d8720d1c0e102f20eb33939fc9137ecc48de
localhorst <uwe.toeppe.mentoring@gmail.com>
2023-11-11 19:56:05 +0100

    line of sight tweaking. and its green

--- elite_components/elite_pixel_applications/line_of_sight_demo/line_of_sight_demo.h
+++ elite_components/elite_pixel_applications/line_of_sight_demo/line_of_sight_demo.h
@@ -10,6 +10,7 @@ typedef struct{
     float fy;
     float fvy;
     float fvx;
+    float fspeed;
     sfRGBA core_color;
     sfRGBA light_color;
     float fr;
@@ -41,6 +42,8 @@ bool line_of_sight_demo_on_user_update_pre_particle_shower_update_log=false;
 
 //void light_source_cast_ray(light_source_t* ls,int x,int y){
 void light_source_cast_ray(light_source_t *ls,int x,int y){
+
+    int illumination_value=1;
     int x1=(int)ls->fx;
     int y1=(int)ls->fy;
     int x2=x;
@@ -54,7 +57,7 @@ void light_source_cast_ray(light_source_t *ls,int x,int y){
     if (dx == 0 && dy == 0) {
 
         if ((x1>=0)&&(y1>=0)&&(x1<10)&&(y1<30)) {
-          ls->illumination_map[x][y]=1;
+          ls->illumination_map[x][y]=illumination_value;
             //elite_pixel_game_fputpixelRGBA(ente,x1,y1,c);
           //  c.fr*=0.7f;
 //            c.fg*=0.7f;
@@ -70,26 +73,28 @@ void light_source_cast_ray(light_source_t *ls,int x,int y){
         if (x1 > x2) vx*=-1;
         int x=x1;
         do {
+            x+=vx;
             int y = dy*(x - x1)/dx + y1;
             if ((x>=0)&&(y>=0)&&(x<10)&&(y<30)) {
-                if (ls->illumination_map[x][y]<0) return;
-                ls->illumination_map[x][y]=1;
+                if (ls->illumination_map[x][y]<0) {return;illumination_value=+1;};
+                ls->illumination_map[x][y]=illumination_value;
 
             }
-            x+=vx;
+
         }while (x!=x2);
 
     } else {
         if (y1 > y2) vy*=-1;
         int y=y1;
         do  {
+            y+=vy;
             int x = dx*(y - y1)/dy + x1;
             if ((x>=0)&&(y>=0)&&(x<10)&&(y<30)) {
-              if (ls->illumination_map[x][y]<0) return;
-              ls->illumination_map[x][y]=1;
+              if (ls->illumination_map[x][y]<0) {return;illumination_value=+1;};
+              ls->illumination_map[x][y]=illumination_value;
 
             };
-            y+=vy;
+
         }while (y!=y2);
 
     };
@@ -105,10 +110,10 @@ void line_of_sight_demo_cast_illumination_map(line_of_sight_demo_t* self,light_s
     };
 
 
-    for (int cursor_x=0;cursor_x<10;cursor_x++) {light_source_cast_ray(ls,cursor_x,0);};
+    for (int cursor_x=0;cursor_x<10;cursor_x++) {light_source_cast_ray(ls,cursor_x,-1);};
     for (int cursor_y=0;cursor_y<30;cursor_y++) {light_source_cast_ray(ls,10,cursor_y);};
     for (int cursor_x=0;cursor_x<10;cursor_x++) {light_source_cast_ray(ls,cursor_x,30);};
-    for (int cursor_y=0;cursor_y<30;cursor_y++) {light_source_cast_ray(ls,0,cursor_y);};
+    for (int cursor_y=0;cursor_y<30;cursor_y++) {light_source_cast_ray(ls,-1,cursor_y);};
 
 
 
@@ -133,42 +138,53 @@ line_of_sight_demo_t* line_of_sight_demo_construct(elite_pixel_game_t* ente){
   elog("INFO : [line_of_sight_demo_construct] successfully constructed self(line_of_sight_demo_t); returning self from constructor\n");
   vTaskDelay(log_delay / portTICK_PERIOD_MS);
   sfRGBA c={255.0f,172.0f,142.0f,255.0f};
-  float f_bla=0.4f;
+  float f_bla=0.55f;
   //sfRGBA c2={172.0f*f_bla,112.0f*f_bla,42.0f*f_bla,172.0f};
-  sfRGBA c2={96.0f*f_bla,68.0f*f_bla,172.0f,152.0f};
+//  sfRGBA c2={96.0f*f_bla,68.0f*f_bla,192.0f,172.0f};
+  sfRGBA c2={64.0f*f_bla,192.0f,68.0f*f_bla,152.0f};
   self->light_source.core_color=c;
   self->light_source.light_color=c2;
   self->light_source.fx=0.0f;
   self->light_source.fy=0.0f;
-  self->light_source.fvx=+7.5f;
+  self->light_source.fspeed=+6.25f;
+  self->light_source.fvx=self->light_source.fspeed;
   self->light_source.fvy=0.0f;
-  self->light_source.fr=12.0f;
-
+  self->light_source.fr=15.0f;
+uint8_t map_[30][10]={{0,0,0,0,0,0,0,0,0,0},//0
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,1,1,1,1,1,1,0,0},//4
+                      {0,0,1,1,1,1,1,1,0,0},
+                      {0,0,0,0,0,0,1,1,0,0},
+                      {0,0,0,0,0,0,1,1,0,0},
+                      {0,0,0,0,0,0,1,1,0,0},//9
+                      {0,0,0,0,0,0,1,1,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},//14
+                      {0,0,0,0,1,1,0,0,0,0},
+                      {0,0,0,0,1,1,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,1,1,0,0},//19
+                      {0,0,0,0,0,0,1,1,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},
+                      {0,0,1,1,0,0,0,0,0,0},//24
+                      {0,0,1,1,1,1,1,1,0,0},
+                      {0,0,1,1,1,1,1,1,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0,0}};//29
   for (int y=0;y<30;y++){
       for (int x=0;x<10;x++){
-          self->map[x][y]=0;
+          self->map[x][y]=map_[y][x];
       };
   };
-  self->map[4][5]=1;
-  self->map[4][5]=1;
-  self->map[4][5]=1;
-  self->map[4][6]=1;
-  self->map[3][6]=1;
-  self->map[2][6]=1;
-  self->map[4][13]=1;
-  self->map[5][13]=1;
-  self->map[6][13]=1;
-  self->map[7][13]=1;
-  self->map[3][14]=1;
-  self->map[4][14]=1;
-  self->map[5][14]=1;
-  self->map[6][14]=1;
-  self->map[5][23]=1;
-  self->map[5][23]=1;
-  self->map[5][23]=1;
-  self->map[5][24]=1;
-  self->map[6][24]=1;
-  self->map[7][24]=1;
 
   return self;
 };
@@ -181,91 +197,107 @@ bool line_of_sight_demo_on_user_update(void* params,elite_pixel_game_t *ente,flo
         vTaskDelay(log_delay / portTICK_PERIOD_MS);
         };
 //body
-  line_of_sight_demo_t *self=(line_of_sight_demo_t*)params;
 
-  float fdx=self->light_source.fvx*fElapsedTime;
-  float fdy=self->light_source.fvy*fElapsedTime;
+    line_of_sight_demo_t *self=(line_of_sight_demo_t*)params;
 
+    float fdx=self->light_source.fvx*fElapsedTime;
+    float fdy=self->light_source.fvy*fElapsedTime;
+    bool bounce=false;
 
-/*  if ((self->light_source.fx+fdx>=(float)ente->screen_width)||(self->light_source.fx+fdx<=0.0f)) {
-    self->light_source.fvx*=-1.0f;
-  }else {self->light_source.fx+=fdx;};
+    if (self->light_source.fx+fdx>(float)ente->screen_width-1.0f){
+        self->light_source.fvx=0.0f;
+        self->light_source.fvy=self->light_source.fspeed;
+        bounce=true;
+    };
 
-  if ((self->light_source.fy+fdy>=(float)ente->screen_height)||(self->light_source.fy+fdy<=0.0f)) {
-    self->light_source.fvy*=-1.0f;
-  }else {self->light_source.fy+=fdy;};
-*/
-bool bounce=false;
-  if (self->light_source.fx+fdx>(float)ente->screen_width-1.0f){
-    self->light_source.fvx=0.0f;
-    self->light_source.fvy=7.5f;
-    bounce=true;
+    if (self->light_source.fy+fdy>(float)ente->screen_height-1.0f){
+        self->light_source.fvx=-1.0f*self->light_source.fspeed;
+        self->light_source.fvy=0.0f;
+        bounce=true;
+    };
 
-  };
-  if (self->light_source.fy+fdy>(float)ente->screen_height-1.0f){
-    self->light_source.fvx=-7.5f;
-    self->light_source.fvy=0.0f;
-    bounce=true;
-  };
-  if (self->light_source.fx+fdx<0.0f){
-    self->light_source.fvx=0.0f;
-    self->light_source.fvy=-7.5f;
-    bounce=true;
-  };
-  if (self->light_source.fy+fdy<0.0f){
-    self->light_source.fvx=7.5f;
-    self->light_source.fvy=0.0f;
-    bounce=true;
-  };
+    if (self->light_source.fx+fdx<0.0f){
+        self->light_source.fvx=0.0f;
+        self->light_source.fvy=-1.0f*self->light_source.fspeed;
+        bounce=true;
+    };
 
-if (bounce==false) {self->light_source.fx+=fdx;self->light_source.fy+=fdy;};
+    if (self->light_source.fy+fdy<0.0f){
+        self->light_source.fvx=self->light_source.fspeed;
+        self->light_source.fvy=0.0f;
+        bounce=true;
+    };
 
-  elite_pixel_game_set_target_layer(ente,0);
-  sfRGBA c={0.0f,0.0f,0.0f,255.0f};
-  elite_pixel_game_fill_flayerRGBA(ente,c);
-line_of_sight_demo_cast_illumination_map(self,&self->light_source);
+    if (bounce==false) {self->light_source.fx+=fdx;self->light_source.fy+=fdy;};
+
+    elite_pixel_game_set_target_layer(ente,0);
+    sfRGBA c={0.0f,0.0f,0.0f,255.0f};
+    elite_pixel_game_fill_flayerRGBA(ente,c);
+
+    line_of_sight_demo_cast_illumination_map(self,&self->light_source);
+    elite_pixel_game_draw_filled_circle(ente,self->light_source.fx,self->light_source.fy,1.0f,self->light_source.core_color);
 
 
-  for (int y=0;y<30;y++){
-    for (int x=0;x<10;x++){
+    for (int y=0;y<30;y++){
+        for (int x=0;x<10;x++){
+            float dx=(float)x-self->light_source.fx;
+            float dy=(float)y-self->light_source.fy;
+            float distance=sqrt(dx*dx+dy*dy);
+            sfRGBA c_light;
+            c_light.fr=self->light_source.light_color.fr;
+            c_light.fg=self->light_source.light_color.fg;
+            c_light.fb=self->light_source.light_color.fb;
+            c_light.fa=self->light_source.light_color.fa;
+            sfRGBA c_shadow;
+            c_shadow.fr=c_light.fr*0.25f;
+            c_shadow.fg=c_light.fg*0.25f;
+            c_shadow.fb=c_light.fb*0.25f;
+            c_shadow.fa=c_light.fa*0.45f;
+
+            float rel_dist=distance/self->light_source.fr;
+            c_shadow.fa*=(1.0f-rel_dist);
+            if (c_shadow.fa<=1.0f) c_shadow.fa=1.0f;
+            if (c_shadow.fa>=255.0f) c_shadow.fa=255.0f;
+            elite_pixel_game_fputpixelRGBA(ente,x,y,c_shadow);
+
+            if (rel_dist<1.0f&&rel_dist>0.0f) {
+                //float fmod2=(1.0f-rel_dist)*(1.0f-rel_dist);
+                float fmod=(1.0f-rel_dist);
+                if (fmod<=0.0f) fmod=0.0f;
+                if (fmod>1.0f) fmod=1.0f;
+                c_light.fr*=((0.5f+0.5f)*fmod);
+                c_light.fg*=((0.5f+0.5f)*fmod);
+                c_light.fb*=((0.5f+0.5f)*fmod);
+                c_light.fa*=(fmod);
+                //c_shadow.fa*=fmod_a;
+                if (self->light_source.illumination_map[x][y]>0){
+                    elite_pixel_game_fputpixelRGBA(ente,x,y,c_light);
+                };
+
+                /*sfRGBA c_local;
+                c_local.fr=8.0f;
+                c_local.fg=16.0f;
+                c_local.fg=64.0f;*/
+            };
+        };
+    };
+    for (int y=0;y<30;y++){
+        for (int x=0;x<10;x++){
             if (self->map[x][y]>0) {
-                sfRGBA c={64.0f,96.0f,255.0f,255.0};
+                sfRGBA c={192.0f,96.0f,24.0f,255.0};
                 elite_pixel_game_fputpixelRGBA(ente,x,y,c);
             }
         }
     }
+    if (line_of_sight_demo_on_user_update_leaving_log==false) {
+        line_of_sight_demo_on_user_update_leaving_log=true;
+        elog("INFO : [line_of_sight_demo_on_user_update] leaving line_of_sight_demo_on_user_update() - this notification will only occur once\n");
+        vTaskDelay(log_delay / portTICK_PERIOD_MS);
+    };
 
-  elite_pixel_game_draw_filled_circle(ente,self->light_source.fx,self->light_source.fy,1.5f,self->light_source.core_color);
+    return true;
+};
 
-  for (int y=0;y<30;y++){
-    for (int x=0;x<10;x++){
-            if (self->light_source.illumination_map[x][y]>0) {
-                float dx=(float)x-self->light_source.fx;
-                float dy=(float)y-self->light_source.fy;
-                float distance=sqrt(dx*dx+dy*dy);
-                sfRGBA c_local;
-                c_local.fr=self->light_source.light_color.fr;
-                c_local.fg=self->light_source.light_color.fg;
-                c_local.fb=self->light_source.light_color.fb;
-                c_local.fa=self->light_source.light_color.fa;
-                float rel_dist=distance/self->light_source.fr;
-                if (rel_dist<1.0f&&rel_dist>0.0f) {
-                  float fmod2=(1.0f-rel_dist)*(1.0f-rel_dist);
-                  float fmod=(1.0f-rel_dist);
-              /*  c_local.fr*=fmod;
-                c_local.fg*=fmod;
-                c_local.fb*=fmod;*/
-                c_local.fa*=fmod2;
-
-        /*    sfRGBA c_local;
-            c_local.fr=8.0f;
-            c_local.fg=16.0f;
-            c_local.fg=64.0f;*/
-                elite_pixel_game_fputpixelRGBA(ente,x,y,c_local);
-                };
-            };
-    }
-  }
 
 /*  int _x=(int)self->light_source.fx;
   int _y=(int)self->light_source.fy;
@@ -301,14 +333,7 @@ line_of_sight_demo_cast_illumination_map(self,&self->light_source);
   }
 */
 //debug tracing out
-  if (line_of_sight_demo_on_user_update_leaving_log==false) {
-      line_of_sight_demo_on_user_update_leaving_log=true;
-      elog("INFO : [line_of_sight_demo_on_user_update] leaving line_of_sight_demo_on_user_update() - this notification will only occur once\n");
-      vTaskDelay(log_delay / portTICK_PERIOD_MS);
-    };
 
-  return true;
-};
 
 
 
